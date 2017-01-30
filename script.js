2a9c91f73e3e7ba78a52312ac5baf05d5587da48
Shruti Kapoor <shruti.kapoor@telus.com>
2017-01-29 22:50:19 -0800

    working version of quotes

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $("#quoteGenerator").on("click", function(){
-    var jsonUrl ="http://quotes.stormconsultancy.co.uk/random.json";
-      
-    $.getJSON(jsonUrl,function(json){
-      var quote = json.quote;
-      var author = json.author;
-      var html = "";
-      
-      html+= "<h3>" + JSON.stringify(quote) + "</h3>";
-      html+= "<em>" + JSON.stringify(author) + "</p>"; 
-        $("#randomQuote").html(html); 
-      });
-    
-    
-      $("#randomQuote").html("<p>New Quote Generated </p>");
+  var jsonUrl = "http://quotes.stormconsultancy.co.uk/random.json";
+
+  $.getJSON(jsonUrl, function(json) {
+    var quote = json.quote;
+    var author = json.author;
+    var html = "";
+
+    html += "<h3 class='quote'>" + quote + "</h3>";
+    html += "<p class='author'> <em> - " + author + "</em></p>";
+    $("#randomQuote").html(html);
+  });
+
   });
 });
